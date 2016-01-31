76b9aa01a354ee482223e58baefe3cae13c50bae
John LaGreco <johnlagreco@gmail.com>
2016-01-30 23:41:09 -0500

    Changed to simpsons theme. Functional JS play, pause & stop buttons.

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ document.getElementById('stopSong').addEventListener("click", function(){
 	jukeBox.stop();
 });
 
+document.getElementById('nextSong').addEventListener("click", function(){
+	jukeBox.next();
+});
+
 var jukeBox = {
+	currentSong: 0,
 	songs: ["song1", "song2", "song3"],
 	play: function(){
 		document.getElementById(this.songs[0]).play();
@@ -21,13 +26,25 @@ var jukeBox = {
 	},
 
 	stop: function(){
-		document.getElementById(this.songs[0]).stop();
-	}
+		document.getElementById(this.songs[0]).pause();
+		document.getElementById(this.songs[0]).currentTime = 0;
+	},
+
+	next: function(){
+		if(this.currentSong == this.songs.length - 1){
+			this.currentSong = 0
+			document.getElementById(this.songs[0]).setAttribute('src', this.songs[0]),
+			jukeBox.play();
+		} else{
+			this.currentSong ++
+			document.getElementById(this.songs[0]).setAttribute('src', this.songs[this.currentSong]),
+			jukeBox.play();
+		}
+	},
 
 };
 
 // JukeBox.loadSong(1);
-// JukeBox.stop();
 
 
 
